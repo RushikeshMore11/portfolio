f48e7ab641914ff4ddd8fdf6868b80d46239d517
rushikesh-scalex <rushikeshm@scalextechnologies.com>
2023-11-21 23:06:50 +0530

    feat: eslint add
    scss add

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hello there
-      </h1>
+      <h1>Hello there</h1>
     </div>
   );
 }
